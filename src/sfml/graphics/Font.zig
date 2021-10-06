dcfeb707b8f4b0db19c54b75226d8a5fd9e80540
Guillaume DEREX <gderex8@orange.fr>
2021-10-06 16:17:52 +0200

    Added some font functions

--- src/sfml/graphics/Font.zig
+++ src/sfml/graphics/Font.zig
@@ -18,6 +18,32 @@ pub fn destroy(self: *Font) void {
     sf.c.sfFont_destroy(self._ptr);
 }
 
+/// Gets the family name of this font
+/// Normally, this is done through getInfo, but as info only contains this data, this makes more sense
+pub fn getFamily(self: Font) [*:0]const u8 {
+    return sf.c.sfFont_getInfo(self._ptr).family;
+} 
+
+/// Gets the kerning offset of two glyphs
+pub fn getKerning(self: Font, first: u32, second: u32, character_size: usize) f32 {
+    return sf.c.sfFont_getKerning(self._ptr, first, second, @intCast(c_uint, character_size));
+}
+
+/// Gets the default spacing between two lines
+pub fn getLineSpacing(self: Font, character_size: usize) f32 {
+    return sf.c.sfFont_getLineSpacing(self._ptr, @intCast(c_uint, character_size));
+}
+
+/// Gets the vertical offset of the underline
+pub fn getUnderlinePosition(self: Font, character_size: usize) f32 {
+    return sf.c.sfFont_getUnderlinePosition(self._ptr, @intCast(c_uint, character_size));
+}
+/// Gets the underline thickness
+pub fn getUnderlineThickness(self: Font, character_size: usize) f32 {
+    return sf.c.sfFont_getUnderlineThickness(self._ptr, @intCast(c_uint, character_size));
+}
+
+pub const getGlyph = @compileError("Function is not implemented yet.");
 pub const initFromMemory = @compileError("Function is not implemented yet.");
 pub const initFromStream = @compileError("Function is not implemented yet.");
 
