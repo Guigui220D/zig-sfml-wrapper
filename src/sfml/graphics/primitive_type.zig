84ec70aef9adc6cf39845b802dc4ab3d9bd495d1
guillaumederex <gderex8@orange.fr>
2022-01-16 10:10:30 +0100

    Made primitive iteration (fixed #33 probably)

--- src/sfml/graphics/primitive_type.zig
+++ src/sfml/graphics/primitive_type.zig
@@ -1,5 +1,7 @@
 //! The type of primitives in a vertex array of buffer
 
+const Vertex = @import("vertex.zig").Vertex;
+
 pub const PrimitiveType = enum(c_uint) {
     Points,
     Lines,
@@ -8,4 +10,48 @@ pub const PrimitiveType = enum(c_uint) {
     TriangleStrip,
     TriangleFan,
     Quads,
+
+    /// Gives the corresponding primitive type for primitive iteration
+    /// See Vertex.verticesAsPrimitives()
+    pub fn Type(comptime primitive_type: PrimitiveType) type {
+        return switch (primitive_type) {
+            .Points => PointPrimitive,
+            .Lines => LinePrimitive,
+            .Triangles => TrianglePrimitive,
+            .Quads => QuadPrimitive,
+            else => @compileError("Primitive type not supported")
+        };
+    }
+    /// Says how many vertices each primitive is composed of
+    pub fn packedBy(primitive_type: PrimitiveType) usize {
+        return switch (primitive_type) {
+            .Points => 1,
+            .Lines => 2,
+            .Triangles => 3,
+            .Quads => 4,
+            else => @panic("Primitive type not supported")
+        };
+    }
+    /// A single point
+    pub const PointPrimitive = packed struct {
+        a: Vertex
+    };
+    /// A line between two points
+    pub const LinePrimitive = packed struct {
+        a: Vertex,
+        b: Vertex
+    };
+    /// A triangle between three points
+    pub const TrianglePrimitive = packed struct {
+        a: Vertex,
+        b: Vertex,
+        c: Vertex
+    };
+    /// A quad between four points
+    pub const QuadPrimitive = packed struct {
+        a: Vertex,
+        b: Vertex,
+        c: Vertex,
+        d: Vertex
+    };
 };
--- src/sfml/graphics/vertex.zig
+++ src/sfml/graphics/vertex.zig
@@ -2,4 +2,25 @@
 
 const sf = @import("../sfml.zig");
 
-pub const Vertex = packed struct { position: sf.system.Vector2f = sf.system.Vector2f{ .x = 0, .y = 0 }, color: sf.graphics.Color = sf.graphics.Color.White, tex_coords: sf.system.Vector2f = sf.system.Vector2f{ .x = 0, .y = 0 } };
+pub const Vertex = packed struct { 
+    /// Position of the vertex
+    position: sf.system.Vector2f = sf.system.Vector2f{ .x = 0, .y = 0 },
+    /// Color of the vertex
+    color: sf.graphics.Color = sf.graphics.Color.White,
+    /// Texture coordinates of the vertex
+    tex_coords: sf.system.Vector2f = sf.system.Vector2f{ .x = 0, .y = 0 },
+
+    /// Allows iterating over a slice of vertices as if they were primitives
+    /// See primitive_type
+    /// Not tested yet
+    pub fn verticesAsPrimitives(vertices: []Vertex, comptime primitive_type: sf.graphics.PrimitiveType) []primitive_type.Type() {
+        if (vertices.len % (comptime primitive_type.packedBy()) != 0)
+            @panic("The total number of vertices must be a multiple of the primitive type number of vertices");
+
+        var ret: []primitive_type.Type() = undefined;
+        ret.len = vertices.len / (comptime primitive_type.packedBy());
+        ret.ptr = @ptrCast([*]primitive_type.Type(), vertices.ptr);
+
+        return ret;
+    }
+};
